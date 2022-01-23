fcf53c23b29867dc5cbf60c4a6f1f9f48dbc1b97
Thomas <tomgrv@users.perspikapps.fr>
2022-01-23 18:29:31 +0100

    chore: start adapting for DSM7

--- .ci/build-package.sh
+++ .ci/build-package.sh
@@ -1,19 +1,52 @@
 #!/bin/bash
+# Copyright (c) 2000-2021 Tomgrv All rights reserved.
 
-set -x
-set -e
+# load toolchain
+source /toolkit/pkgscripts-ng/include/pkg_util.sh
 
-# move build files into source dir
-mkdir -p /toolkit/source
-cp -R $GITHUB_WORKSPACE /toolkit/source/videorenamer
+# init names
+[ -n "$1" ]
+SPK_PACKAGE_NAME=$1
 
-# grap the node.js binary
-cd /toolkit/source/videorenamer
-NODE_LTS="$(curl -s https://nodejs.org/dist/index.json | jq -r 'map(select(.lts))[0].version')"
-wget https://nodejs.org/dist/$NODE_LTS/node-$NODE_LTS-linux-x64.tar.gz -O node-linux-x64.tar.gz
+[ -n "$2" ]
+SPK_PACKAGE_VERSION=$2
 
-# build spk
-/toolkit/pkgscripts-ng/PkgCreate.py -p braswell -c videorenamer
+# set include projects to install into this package
+DSM_DIR="/tmp/_${SPK_PACKAGE_NAME}"      # temp folder for dsm files
+PKG_DIR="/tmp/_${SPK_PACKAGE_NAME}_pkg"   # temp folder for package files
+OUT_DIR="./.spk"   # temp folder for package archive
 
-# copy spk to github workspace
-cp /toolkit/build_env/ds.braswell-7.0/image/packages/videorenamer-braswell-$SPK_PACKAGE_VERSION.spk $GITHUB_WORKSPACE/videorenamer-$SPK_PACKAGE_VERSION.spk
+# prepare install and package dir
+for dir in $DSM_DIR $PKG_DIR; do
+        rm -rf "$dir"
+done
+for dir in $DSM_DIR $PKG_DIR $OUT_DIR; do
+        mkdir -p "$dir" # use default mask
+done
+
+[ -d $DSM_DIR/ui ] || install -d $DSM_DIR/ui
+cp -a ui/* $DSM_DIR/ui
+
+[ -d $DSM_DIR/app ] || install -d $DSM_DIR/app
+cp -a app/* $DSM_DIR/app
+
+[ -d $PKG_DIR ] || install -d $PKG_DIR
+[ -d $PKG_DIR/scripts ] || install -d $PKG_DIR/scripts
+cp -a conf $PKG_DIR
+cp -a scripts/* $PKG_DIR/scripts
+chmod 755 $PKG_DIR/scripts/*
+
+cp -av PACKAGE_ICON*.PNG $PKG_DIR
+
+# force LF
+find $PKG_DIR -type 'f' -exec dos2unix {} \;
+find ./package -type 'f' -exec dos2unix {} \;
+
+# create INFO
+source ./INFO.env >/dev/null
+pkg_dump_info > ${PKG_DIR}/INFO
+
+# pack
+pkg_make_package ./package ${PKG_DIR}
+pkg_make_spk ${PKG_DIR} ${OUT_DIR} ${SPK_PACKAGE_NAME}_${SPK_PACKAGE_VERSION}.spk
+
--- .ci/setup-toolchain.sh
+++ .ci/setup-toolchain.sh
@@ -4,12 +4,10 @@ set -x
 set -e
 
 apt-get update
-apt-get install -y cifs-utils python3 python3-pip jq
+apt-get install -y cifs-utils python3 python3-pip jq xz
 
 mkdir -p /toolkit
 cd /toolkit
 git clone https://github.com/SynologyOpenSource/pkgscripts-ng
 cd /toolkit/pkgscripts-ng/
 git checkout DSM7.0
-
-/toolkit/pkgscripts-ng/EnvDeploy -v 7.0 -p braswell
